06a2f165c03895f1d2ae3ea5b6941b1b5bbc670d
Maksym <max.voloshin.ua@gmail.com>
2015-06-17 16:44:14 +0200

    Start project

---
+++ src/implementations/Computer.java
@@ -0,0 +1,17 @@
+package implementations;
+
+import objects.Field;
+import interfaces.Player;
+import interfaces.Position;
+
+/**
+ * Created by Maksym Voloshyn on 17.06.2015 for TicTacToe project.
+ * All rights reserved
+ */
+public class Computer implements Player {
+
+    @Override
+    public Field move(Position position) {
+        return null;
+    }
+}
